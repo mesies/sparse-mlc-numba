f3ba31806371cf365e85865908861424249f72ce
Napoleon Maraidonis <napoleon_maraidonis@hotmail.com>
2017-07-16 21:18:37 +0300

    Wrong data import type starting to fix

--- core.py
+++ core.py
@@ -4,9 +4,14 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from helpers import load_mlc_dataset
 
+#   Options
 DATASET_FILENAME = "delicious_data.txt"
 DATASET_TRAIN_SET_FILENAME = "delicious_trSplit.txt"
 DATASET_TEST_SET_FILENAME = "delicious_tstSplit.txt"
+
+#   Debug Options
+DEBUG = 1
+DEBUG_DATASET_SIZE = 50
 
 # 1. Load dataset
 #   1.1 Load data from delicious dataset, also use sklearn's sparse data structures.
@@ -16,20 +21,30 @@ X, y, header_info = load_mlc_dataset(DATASET_FILENAME,
 #       contain indexes.
 
 f1 = open(DATASET_TRAIN_SET_FILENAME)
-train_ind = np.loadtxt(fname=f1, delimiter=" ")
+train_ind = np.loadtxt(fname=f1, delimiter=" ", dtype=int)
 f1.close()
 
 f2 = open(DATASET_TEST_SET_FILENAME)
-test_ind = np.loadtxt(fname=f2, delimiter=" ")
+test_ind = np.loadtxt(fname=f2, delimiter=" ", dtype=int)
 f2.close()
 
-X_train = X[train_ind[:, 0]]
-X_test = X[test_ind[:, 0]]
+print(type(y))
+#TODO problem -> y is a list
 
-y_train = y[train_ind[:, 0]]
-y_test = y[test_ind[:, 0]]
+if DEBUG == 1:
+    X_train = X[train_ind[:, 0]]
+    X_test = X[test_ind[:, 0]]
 
+    y_train = y[train_ind[:, 0]]
+    y_test = y[test_ind[:, 0]]
+else:
+    X_train = X[train_ind[:, 0]]
+    X_test = X[test_ind[:, 0]]
 
+    y_train = y[train_ind[:, 0]]
+    y_test = y[test_ind[:, 0]]
+
+print(X_train[19])
 # 2. Implement Binary Logistic Regression Classifier
 #   2.1 Code likelihood
 #   2.2 Code Stochastic Gradient Descent with regards to dataset sparsity
