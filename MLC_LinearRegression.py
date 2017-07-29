Implemented Stochastic Gradient Descent, Fixed gradient check.

MLC_LinearRegression.py
@@ -10,37 +10,39 @@ examples matrix is a sparse matrix
 
 
 class MLC_LinearRegression:
-    def __init__(self, learning_rate=0.001, iterations=1000, debug=True):
-
+    def __init__(self, learning_rate=0.0001, iterations=1000, debug=False, verbose=False, batch_size=20):
+        self.batch_size = batch_size
+        self.verbose = verbose
         self.l = learning_rate
         self.iterations = iterations
         self.w = {}
         self.debug = debug
         self.lossHistory = []
-        if debug:
-            logging.basicConfig(filename=__name__ + '.log', filemode='w', level=logging.DEBUG)
+        if verbose:
+            logging.basicConfig(level=logging.INFO)
         else:
             logging.basicConfig(filename=__name__ + '.log', filemode='w', level=logging.INFO)
 
     def fit(self, X, y):
         logging.info("Started Fitting Dataa")
         self.w = np.random.uniform(size=(X.shape[1],))
-        logging.debug("Commencing Gradient Check")
-        #logging.debug(helpers.grad_check(X, self.w, y))
+        if self.debug:
+            logging.info("Commencing Gradient Check")
+            logging.info(helpers.grad_check(X, self.w, y))
 
-        self.w = self.gradient_decent(X, y, epochs=self.iterations)
+        self.w = self.stochastic_gradient_descent(X, y, epochs=self.iterations, tolerance=1e-3, batch_size=self.batch_size)
         return self.w
 
     def gradient_decent(self, X, y, tolerance=1e-3, epochs=10000):
         old_loss = np.inf
         for epoch in np.arange(0, epochs):
 
-            logging.info("INFO: Commencing next epoch %i", epoch)
+            logging.info("Commencing next epoch %i", epoch)
 
             gradient = helpers.gradient(X, self.w, y)
             loss = helpers.log_likelihood(X, self.w, y)
             self.lossHistory.append(loss)
-            logging.info("INFO: epoch #{}, loss={} , gradient={}".format(epoch + 1, loss, gradient))
+            logging.info("epoch #{}, loss={} , gradient={}".format(epoch + 1, loss, gradient))
 
             if np.abs(loss - old_loss) < tolerance:
                 break
@@ -49,6 +51,39 @@ class MLC_LinearRegression:
             self.w = self.w - self.l * gradient
 
         return self.w
+
+    def stochastic_gradient_descent(self, X, y, tolerance, epochs=2000, batch_size=10):
+        old_loss = np.inf
+        old_loss_ep = np.inf
+        logging.info("Commencing SGD")
+        logging.info("Options : tol = %f, epochs = %f, learning rate = %f", tolerance, epochs, self.l)
+        epochloss = []
+        for epoch in np.arange(0, epochs):
+            old_loss = np.inf
+            for(sampleX, sampley) in self.next_batch(X, y, batch_size):
+
+                loss = helpers.log_likelihood(X=sampleX, y=sampley.T, W=self.w)
+
+                epochloss.append(loss)
+                if np.abs(loss - old_loss) < tolerance:
+                    break
+                old_loss = loss
+
+                gradient = helpers.gradient(sampleX, self.w, sampley)
+
+                self.w = self.w - self.l * gradient
+            self.lossHistory.append(np.average(epochloss))
+            logging.info("Ening epoch %i, average loss -> %f", epoch, np.average(epochloss))
+
+            # if np.abs(np.average(epochloss) - old_loss_ep):
+            #     break
+            # old_loss_ep = np.average(epochloss)
+
+        return self.w
+
+    def next_batch(self, X, y, batchSize=5):
+        for i in np.arange(0, X.shape[0], batchSize):
+            yield (X[i:i+batchSize], y[i:i+batchSize])
 
     def predict(self, X):
         logging.info("Predicting Labels")

helpers.py
@@ -1,7 +1,6 @@
 
-import autograd
+#import autograd
 import logging
-import numpy as npp
 import math
 import time
 import numpy as np
@@ -113,50 +112,11 @@ def log_likelihood(X, W, y):
     :param y: True Categories of the training examples X
     :return: minus log likelihood
     """
-    logging.basicConfig(filename=__name__ + '.log', filemode='w', level=logging.INFO)
-
-    t = y
-
-    xw_hat = ((-1)**(1-t)) * np.dot(X, W)
+    sign = (-1)**(1-y)
+    xw_hat = (sign) * np.dot(X, W)
     L = -np.sum(-np.log(1 + np.exp(-xw_hat)))
-    wut = np.zeros((X.shape))
-    for n in np.arange(0, X.shape[0]):
-        for k in np.arange(0, W.shape[0]):
-            wut[n][k] = X[n][k] * W[k]
-    L1 = -np.sum(-(logsumexp(-wut)))
-    print L
-    print L1
-    return L1
 
-
-def logsumexp(seq):
-    #print np.log(1 + np.exp(-seq))
-    varr = np.c_[seq, np.zeros(seq.shape[0]).T]
-    logging.info("-wut")
-    logging.info(seq)
-    logging.info("-var")
-    logging.info(varr)
-    maxxx = np.max(varr, axis=1)
-    logging.info("max")
-    logging.info(maxxx)
-
-    #print str(maxx) + " kappa"
-    maxx = np.array([maxxx] * (varr.shape[1]))
-    logging.info("max")
-    logging.info(maxx)
-
-
-    summ = np.sum(np.exp(np.subtract(varr, maxx.T)), axis=0)
-    logging.info("summ")
-    logging.info(summ.shape)
-    logging.info(summ)
-
-    logging.info("returns")
-    logging.info(((maxxx) + np.log(summ)).shape)
-    logging.info((maxxx) + np.log(summ))
-    return (maxxx) + np.log(summ)
-
-
+    return (L)
 
 def auto_gradient(X, W, y):
     """
@@ -179,12 +139,7 @@ def gradient(X, W, y):
     :return: Gradient
     """
     # old_grad = - (X.T.dot(y)) + (X.T.dot(X)).dot(W)
-
-    sigm_xw = sigmoid(np.dot(X, W))
-
-    neo = np.sum((sigm_xw.T - y) * X.T, axis=1)
-
-    return neo
+    return np.sum(((sigmoid(np.dot(X, W))).T - y) * X.T, axis=1)
 
 
 def gradient_precalc(X, sigm_xw, y):
@@ -207,26 +162,24 @@ def grad_check(X, W, y):
     :param W: Weight vector
     :param y: True Categories of the training examples X
     """
-    # Works but the numeric gradient is double than the actual gradient for some reason
 
     true_gradient = gradient(X, W, y)
 
-    epsilon = 1e-4
+    epsilon = 1e-6
     num_grad = np.zeros(W.shape[0])
 
     for k in np.arange(0, W.shape[0]):
-        W_tmp = W
+        W_tmp = np.array(W)
 
-        W_tmp[k] = W_tmp[k] + epsilon
+        W_tmp[k] = W_tmp[k] + (epsilon)
         Ewplus = log_likelihood(X, W_tmp, y)
 
-        W_tmp = W
+        W_tmp = np.array(W)
 
-        W_tmp[k] = W_tmp[k] - epsilon
+        W_tmp[k] = W_tmp[k] - (epsilon)
         Ewminus = log_likelihood(X, W_tmp, y)
 
-
-        num_grad[k] = np.divide((np.subtract(Ewplus, Ewminus)), np.multiply(1, epsilon))
+        num_grad[k] = np.divide((np.subtract(Ewplus, Ewminus)), np.multiply(2, epsilon))
 
     true_gradient.reshape((W.shape[0], 1))
     num_grad.reshape((W.shape[0], 1))
@@ -239,8 +192,7 @@ def grad_check(X, W, y):
 
 def plot_linreg_results(X, W, y, preds, lossHistory):
 
-
-    print("Score " + str(accuracy_score(y_true=y, y_pred=preds)))
+    #print("Score " + str(accuracy_score(y_true=y, y_pred=preds)))
 
     Y = (-W[0] - (W[1] * X)) / W[2]
 

test.py
@@ -8,8 +8,8 @@ from MLC_LinearRegression import MLC_LinearRegression
 import matplotlib.pyplot as plt
 
 (X, y) = sklearn.datasets.make_blobs(
-    n_samples=400,
-    n_features=70,
+    n_samples=1500,
+    n_features=500,
     centers=2,
     cluster_std=1.05,
     random_state=20)
@@ -20,11 +20,11 @@ np.float64(X)
 np.float64(y)
 
 
-mlc = MLC_LinearRegression(learning_rate=0.001)
+mlc = MLC_LinearRegression(learning_rate=0.001,iterations=2000, batch_size=200, verbose=True)
 W = mlc.fit(X, y)
 preds = mlc.predict(X)
 
 print("Score " + str(accuracy_score(y_true=y, y_pred=preds)))
 
-#helpers.plot_linreg_results(X, W, y, preds, mlc.lossHistory)
+helpers.plot_linreg_results(X, W, y, preds, mlc.lossHistory)
 